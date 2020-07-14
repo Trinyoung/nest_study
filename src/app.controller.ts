f484b829e3f6d70cb650c6e2c1d220fd347e644b
trinyoung <trinyoung@163.com>
2020-07-14 18:12:33 +0800

    fix(controller): controller使用

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,8 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+  @Get('/haha')
+  getHaHa(): string {
+    return this.appService.getHaha();
+  }
 }
